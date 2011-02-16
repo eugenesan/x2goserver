72460da5e87334334671942039351e09ea8cc857
Reinhard Tartler <siretart@tauware.de>
2011-02-16 08:47:26 +0100

    cleanup example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the x2goserver package
-#
-0 4	* * *	root	x2goserver_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(x2goserver):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="x2goserver" command="/usr/bin/x2goserver"
