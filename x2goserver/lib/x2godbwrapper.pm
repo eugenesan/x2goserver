2359295f4a9b91e4be3a464b35f031549e3ddb78
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2011-07-28 15:05:01 +0200

    Add x2gobasepath script to reliably detect x2goserver install path.

--- x2goserver/lib/x2godbwrapper.pm
+++ x2goserver/lib/x2godbwrapper.pm
@@ -26,7 +26,7 @@ use DBI;
 
 use POSIX;
 
-my $x2go_lib_path=`echo -n \$(dirname $0)`;
+my $x2go_lib_path=`echo -n $(x2gobasepath)/lib/x2go`;
 
 my ($uname, $pass, $uid, $pgid, $quota, $comment, $gcos, $homedir, $shell, $expire) = getpwuid(getuid());
 
