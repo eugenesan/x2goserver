d34d3656f53de76f961f5c85feb1882b98effab8
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2012-09-17 13:08:19 +0200

    add forgotten files for X2Go::Server package

---
+++ X2Go/Server.pm
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+# Copyright (C) 2007-2012 X2Go Project - http://wiki.x2go.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Copyright (C) 2007-2012  Oleksandr Shneyder <oleksandr.shneyder@obviously-nice.de>
+# Copyright (C) 2007-2012  Heinz-Markus Graesing <heinz-m.graesing@obviously-nice.de>
+
+package X2Go::Server;
+
+=head1 NAME
+
+X2Go::Server - X2Go Server package for Perl
+
+=head1 DESCRIPTION
+
+X2Go::Server Perl package.
+
+=cut
+
+use strict;
+use base 'Exporter';
+
+1;
